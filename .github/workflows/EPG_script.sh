f4fe5214dc7effb298aa9c925f550e979a4c4f2d
David <davidmumar@gmail.com>
2024-10-08 00:54:06 +0200

    Add files via upload

--- .github/workflows/EPG_script.sh
+++ .github/workflows/EPG_script.sh
@@ -4,11 +4,11 @@ curl -L -o guiaiptv.xml "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/
 
 sed -i ':a; N; $!ba; s/<title lang=\"es\">\n/<title lang=\"es\">/g' guiaiptv.xml
 
-sed -i '/^ *$/d' nomxml.txt
-sed -i '/-INICIO,FICHERO-/d' nomxml.txt
-sed -i '/-FIN,FICHERO-/d' nomxml.txt
-sed -i '1i -INICIO,FICHERO-' nomxml.txt
-sed -i '$a -FIN,FICHERO-' nomxml.txt
+sed -i '/^ *$/d' canales.txt
+sed -i '/-INICIO,FICHERO-/d' canales.txt
+sed -i '/-FIN,FICHERO-/d' canales.txt
+sed -i '1i -INICIO,FICHERO-' canales.txt
+sed -i '$a -FIN,FICHERO-' canales.txt
 
 date_stamp=$(date +"%d/%m/%Y %R")
 echo '<?xml version="1.0" encoding="UTF-8"?>' > EPG_personal.xml
@@ -20,14 +20,14 @@ echo "<tv generator-info-name=\"dobleM $date_stamp\" generator-info-url=\"t.me/E
 		sed -n '/<channel id=\"'$old'"/,/<\/channel>/p' EPG_personal.xml >> EPG_personal.xml
 		sed -i "s/$old/$new/" EPG_personal.xml
 		sed -i "s#$new<\/display-name>#$new<\/display-name>\n\t<icon src=\"$logo\" \/>#" EPG_personal.xml
-	done < nomxml.txt
+	done < canales.txt
 	
 	while IFS=, read -r old new logo
 	do
 		echo Procesando programme: $old ··· $new
 		sed -n '/<programme.*"'$old'">/,/<\/programme>/p' EPG_personal.xml >> EPG_personal.xml
 		sed -i "s/channel=\"$old\"/channel=\"$new\"/" EPG_personal.xml
-	done < nomxml.txt
+	done < canales.txt
 
 echo '</tv>' >> EPG_personal.xml
 
