9ef01c735a6c3795ef842448272d02c1b9633a03
David <davidmumar@gmail.com>
2024-10-09 00:47:46 +0200

    Add files via upload

--- .github/workflows/EPG_script.sh
+++ .github/workflows/EPG_script.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-#curl -L -o EPG_temp.xml "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guiaiptv.xml"
-
-sed -i ':a; N; $!ba; s/<title lang=\"es\">\n/<title lang=\"es\">/g' EPG_temp.xml
+curl -L -o .github/workflows/EPG_temp.xml "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guiaiptv.xml"
 
 sed -i '/^ *$/d' canales.txt
-sed -i '/-INICIO,FICHERO-/d' canales.txt
-sed -i '/-FIN,FICHERO-/d' canales.txt
-sed -i '1i -INICIO,FICHERO-' canales.txt
-sed -i '$a -FIN,FICHERO-' canales.txt
+
+	while IFS=, read -r old new logo
+	do
+		contar_channel="$(grep -c "channel=\"$old\"" .github/workflows/EPG_temp.xml)"
+		if [ $contar_channel -gt 1 ]; then
+			echo Procesando canal: $old ··· $contar_channel coincidencias
+			
+			sed -n "/<channel id=\"${old}\">/,/<\/channel>/p" .github/workflows/EPG_temp.xml >> .github/workflows/EPG_temp1.xml
+			sed -i '/display-name/d' .github/workflows/EPG_temp1.xml
+			sed -i '/icon src/d' .github/workflows/EPG_temp1.xml
+			sed -i "s/${old}/${new}/" .github/workflows/EPG_temp1.xml
+			sed -i "s#<\/channel>#\t<icon src=\"${logo}\" />\n  <\/channel>#" .github/workflows/EPG_temp1.xml
+			
+			sed -n "/<programme.*${old}\">/,/<\/programme>/p" .github/workflows/EPG_temp.xml >> .github/workflows/EPG_temp2.xml
+			sed -i "s# channel=\"${old}\"# channel=\"${new}\"#" .github/workflows/EPG_temp2.xml
+			
+		else
+			echo Saltando canal: $old ··· $contar_channel coincidencias
+		fi	
+	done < canales.txt
 
 date_stamp=$(date +"%d/%m/%Y %R")
-echo '<?xml version="1.0" encoding="UTF-8"?>' > EPG_personal.xml
-echo "<tv generator-info-name=\"dobleM $date_stamp\" generator-info-url=\"t.me/EPG_dobleM\">" >> EPG_personal.xml
+echo '<?xml version="1.0" encoding="UTF-8"?>' > miEPG.xml
+echo "<tv generator-info-name=\"miEPG $date_stamp\" generator-info-url=\"t.me/miEPG\">" >> miEPG.xml
+cat .github/workflows/EPG_temp1.xml >> miEPG.xml
+cat .github/workflows/EPG_temp2.xml >> miEPG.xml
+echo '</tv>' >> miEPG.xml
 
-	while IFS=, read -r old new logo
-	do
-		echo Procesando channel y logos: $old ··· $new ··· $logo
-  		grep "<channel id=\"$old\">" EPG_temp.xml >> EPG_personal.xml
-		#sed -n "/<channel id=\"$old\">/,/<\/channel>/p" EPG_temp.xml >> EPG_personal.xml
-  		#sed -i '/display-name/d' EPG_personal.xml
-  		#sed -i '/icon src/d' EPG_personal.xml
-    		sed -i "s/$old/$new/" EPG_personal.xml
-		#sed -i 's|</channel>|\t<icon src=\"'"$log"'\" />\n\t</channel>|' EPG_personal.xml
-	done < canales.txt
-	
-	while IFS=, read -r old new logo
-	do
-		echo Procesando programme: $old ··· $new
-		sed -n "/<programme.*$old/,/<\/programme>/p" EPG_temp.xml >> EPG_personal.xml
-		sed -i "s|channel=\"$old\"|channel=\"$new\"/" EPG_personal.xml
-	done < canales.txt
-
-echo '</tv>' >> EPG_personal.xml
+rm -f .github/workflows/EPG_temp1.xml
+rm -f .github/workflows/EPG_temp2.xml
