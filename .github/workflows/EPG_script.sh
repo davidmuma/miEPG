63642acbecf441ec2e1dc655d4f37dc198c7511a
David <davidmumar@gmail.com>
2024-10-09 00:49:28 +0200

    Add files via upload

--- .github/workflows/EPG_script.sh
+++ .github/workflows/EPG_script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-curl -L -o .github/workflows/EPG_temp.xml "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guiaiptv.xml"
+curl -L -o EPG_temp.xml "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guiaiptv.xml"
 
 sed -i '/^ *$/d' canales.txt
 
 	while IFS=, read -r old new logo
 	do
-		contar_channel="$(grep -c "channel=\"$old\"" .github/workflows/EPG_temp.xml)"
+		contar_channel="$(grep -c "channel=\"$old\"" EPG_temp.xml)"
 		if [ $contar_channel -gt 1 ]; then
 			echo Procesando canal: $old ··· $contar_channel coincidencias
 			
-			sed -n "/<channel id=\"${old}\">/,/<\/channel>/p" .github/workflows/EPG_temp.xml >> .github/workflows/EPG_temp1.xml
-			sed -i '/display-name/d' .github/workflows/EPG_temp1.xml
-			sed -i '/icon src/d' .github/workflows/EPG_temp1.xml
-			sed -i "s/${old}/${new}/" .github/workflows/EPG_temp1.xml
-			sed -i "s#<\/channel>#\t<icon src=\"${logo}\" />\n  <\/channel>#" .github/workflows/EPG_temp1.xml
+			sed -n "/<channel id=\"${old}\">/,/<\/channel>/p" EPG_temp.xml >> EPG_temp1.xml
+			sed -i '/display-name/d' EPG_temp1.xml
+			sed -i '/icon src/d' EPG_temp1.xml
+			sed -i "s/${old}/${new}/" EPG_temp1.xml
+			sed -i "s#<\/channel>#\t<icon src=\"${logo}\" />\n  <\/channel>#" EPG_temp1.xml
 			
-			sed -n "/<programme.*${old}\">/,/<\/programme>/p" .github/workflows/EPG_temp.xml >> .github/workflows/EPG_temp2.xml
-			sed -i "s# channel=\"${old}\"# channel=\"${new}\"#" .github/workflows/EPG_temp2.xml
+			sed -n "/<programme.*${old}\">/,/<\/programme>/p" EPG_temp.xml >> EPG_temp2.xml
+			sed -i "s# channel=\"${old}\"# channel=\"${new}\"#" EPG_temp2.xml
 			
 		else
 			echo Saltando canal: $old ··· $contar_channel coincidencias
@@ -27,9 +27,10 @@ sed -i '/^ *$/d' canales.txt
 date_stamp=$(date +"%d/%m/%Y %R")
 echo '<?xml version="1.0" encoding="UTF-8"?>' > miEPG.xml
 echo "<tv generator-info-name=\"miEPG $date_stamp\" generator-info-url=\"t.me/miEPG\">" >> miEPG.xml
-cat .github/workflows/EPG_temp1.xml >> miEPG.xml
-cat .github/workflows/EPG_temp2.xml >> miEPG.xml
+cat EPG_temp1.xml >> miEPG.xml
+cat EPG_temp2.xml >> miEPG.xml
 echo '</tv>' >> miEPG.xml
 
-rm -f .github/workflows/EPG_temp1.xml
-rm -f .github/workflows/EPG_temp2.xml
+rm -f EPG_temp.xml
+rm -f EPG_temp1.xml
+rm -f EPG_temp2.xml
